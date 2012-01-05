82661c648d34d73c8ae95a1ea25957c3aedce7b8
Amirhossein Fouladi <amir@Amirhossein-Fouladis-iMac.local>
2012-01-05 08:54:49 +0100

    Fields added to ModeDTO

---
+++ src/se/liu/gwt/widgets/client/ModeDTO.java
@@ -0,0 +1,11 @@
+package 
+public class ModeDTO {
+	String[] keywords;
+	String[] stringCh;
+	String isOperatorChar;
+	String commentSingle;
+	String commentMStart;
+	String commentMEnd;
+	char escapeCh;
+	
+}

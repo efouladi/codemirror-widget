c91220841c5f08e4c875c1f445480574d9a1a93c
amir <ah.fouladi@gmail.com>
2012-11-27 18:10:02 +0100

    again add and remove rest of the files

--- test/se/liu/gwt/widgets/client/CodeMirror2Test.java
+++ test/se/liu/gwt/widgets/client/CodeMirror2Test.java
@@ -1,68 +1,60 @@
 package se.liu.gwt.widgets.client;
 
 
-import com.google.gwt.core.client.GWT;
+import com.google.gwt.json.client.JSONArray;
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONString;
 import com.google.gwt.junit.client.GWTTestCase;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.rpc.ServiceDefTarget;
 
 /**
  * GWT JUnit tests must extend GWTTestCase.
  */
 public class CodeMirror2Test extends GWTTestCase {
 
-  /**
-   * Must refer to a valid module that sources this class.
-   */
-  public String getModuleName() {
-    return "se.liu.gwt.widgets.CodeMirror2JUnit";
-  }
+	/**
+	 * Must refer to a valid module that sources this class.
+	 */
 
-  /**
-   * Tests the FieldVerifier.
-   */
-  public void testFieldVerifier() {
- /*   assertFalse(FieldVerifier.isValidName(null));
-    assertFalse(FieldVerifier.isValidName(""));
-    assertFalse(FieldVerifier.isValidName("a"));
-    assertFalse(FieldVerifier.isValidName("ab"));
-    assertFalse(FieldVerifier.isValidName("abc"));
-    assertTrue(FieldVerifier.isValidName("abcd"));*/
-  }
+	private CodeMirror2 c;
+	public String getModuleName() {
+		return "se.liu.gwt.widgets.CodeMirror2JUnit";
+	}
 
-  /**
-   * This test will send a request to the server using the greetServer method in
-   * GreetingService and verify the response.
-   */
-  public void testGreetingService() {
-    // Create the service that we will test.
-    GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
-    ServiceDefTarget target = (ServiceDefTarget) greetingService;
-    target.setServiceEntryPoint(GWT.getModuleBaseURL() + "codemirror2/greet");
+	
+	public void gwtSetUp(){
+		
+		JSONObject mode = new JSONObject();
+		JSONArray keywords = new JSONArray();
+		JSONArray stringCh = new JSONArray();
+		keywords.set(0,new JSONString("class"));
+		keywords.set(1,new JSONString("System"));
+		stringCh.set(0,new JSONString("\'"));
+		stringCh.set(1,new JSONString("\""));
+		mode.put("name",new JSONString("basemode"));
+		mode.put("keywords",keywords);
+		mode.put("stringCh",stringCh);
+		mode.put("commentSingle",new JSONString("//"));
+		mode.put("commentMStart",new JSONString("/*"));
+		mode.put("commentMEnd",new JSONString("*/"));
+		mode.put("escapeCh",new JSONString("\\"));
+		mode.put("isOperatorChar",new JSONString("\\+"));
+		CodeMirrorConf config = new CodeMirrorConf();
+		
+		config.setMode(mode);
+		config.setLineNumbers(true);
+		
+		c = new CodeMirror2(config);
+		
+	}	
+	public void test_getCursor(){
+		c.onLoad();
+		
+		
+		c.setValue("test\ntest2");
+		c.setCursor(0, 2);
+		assertEquals(0, c.getCursor().getLine().intValue());
+		assertEquals(2, c.getCursor().getCh().intValue());
 
-    // Since RPC calls are asynchronous, we will need to wait for a response
-    // after this test method returns. This line tells the test runner to wait
-    // up to 10 seconds before timing out.
-    delayTestFinish(10000);
-
-    // Send a request to the server.
-    greetingService.greetServer("GWT User", new AsyncCallback<String>() {
-      public void onFailure(Throwable caught) {
-        // The request resulted in an unexpected error.
-        fail("Request failure: " + caught.getMessage());
-      }
-
-      public void onSuccess(String result) {
-        // Verify that the response is correct.
-        assertTrue(result.startsWith("Hello, GWT User!"));
-
-        // Now that we have received a response, we need to tell the test runner
-        // that the test is complete. You must call finishTest() after an
-        // asynchronous test finishes successfully, or the test will time out.
-        finishTest();
-      }
-    });
-  }
-
+	}
 
 }
